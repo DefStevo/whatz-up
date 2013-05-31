bccd69d72e1e21e800fc23f8057a7b444667561b
DefStevo <DefStevo@gmail.com>
2013-05-31 13:17:07 +0000

    Ordner "scripts", "styles" erstellen Datei "scripts/db.s.php", "scripts/globals.s.php" erstellen

---
+++ dev/scripts/globals.s.php
@@ -0,0 +1,14 @@
+<?php
+  // Melde alle PHP Fehler
+  error_reporting(-1);
+
+  //Globale Variablen
+  //Datenbanke
+  $db_server = "localhost";
+  $db_database = "Master";
+  $db_user = "event";
+  $db_pass = "event";
+  
+  //Tabellen
+ 
+?>
